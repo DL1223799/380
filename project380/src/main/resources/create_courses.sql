8308125bd34869fd18d515c28a882df2a0d62b6c
DL1223799 <93562438+DL1223799@users.noreply.github.com>
2022-04-07 18:05:10 +0800

    polling sql

--- project380/src/main/resources/create_courses.sql
+++ project380/src/main/resources/create_courses.sql
@@ -22,6 +22,30 @@ create table Course_user_Comments(
     username VARCHAR(50) NOT NULL,
     PRIMARY KEY (comment_id),
     FOREIGN KEY (username) REFERENCES users(username),
+    FOREIGN KEY (course_id) REFERENCES course(id)
+);
+
+create table Course_polling(
+    polling_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    course_id integer not null,
+    question varchar(255) not null,
+    a varchar(255) not null,
+    b varchar(255) not null,
+    c varchar(255) not null,
+    d varchar(255) not null,
+    poller VARCHAR(50) NOT NULL,
+    PRIMARY KEY (polling_id),
+    FOREIGN KEY (poller) REFERENCES users(username),
+    FOREIGN KEY (course_id) REFERENCES course(id)
+);
+create table Polling_option(
+    option_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    course_id integer not null,
+    polling_id integer not null,
+    opt varchar(255) not null,
+    username VARCHAR(50) NOT NULL,
+    PRIMARY KEY (option_id),
+    FOREIGN KEY (username) REFERENCES users(username),
     FOREIGN KEY (course_id) REFERENCES course(id),
-    FOREIGN KEY (username) REFERENCES users(username)
+    FOREIGN KEY (polling_id) REFERENCES Course_polling(polling_id)
 );
