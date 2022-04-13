63002909eb25f0c1704a18264562385a5257e3ea
andygmd <94505778+andygmd@users.noreply.github.com>
2022-04-14 07:49:22 +0800

    db name project380

---
+++ project380/src/main/java/hkmu/comps380f/exception/CourseOptionNotFound.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hkmu.comps380f.exception;
+
+/**
+ *
+ * @author user
+ */
+public class CourseOptionNotFound extends Exception {
+    
+}
