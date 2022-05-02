9c0f9e8379438abba55b11921345bd1b0ae2a784
andygmd <94505778+andygmd@users.noreply.github.com>
2022-05-02 14:57:14 +0800

    unchange

--- project380/src/main/java/hkmu/comps380f/validator/UserValidator.java
+++ project380/src/main/java/hkmu/comps380f/validator/UserValidator.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package hkmu.comps380f.validator;
 
 import hkmu.comps380f.controller.CourseUserController.Form;
